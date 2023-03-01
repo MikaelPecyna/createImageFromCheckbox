f353d013ed8661bbffc202b5ea93897f9cc5bcd0
Mikael Pecyna <mikael.pecyna@gmail.com>
2023-03-01 13:51:12 +0100

    Changement checkbox vers Label, change color on hover, + Preview de l'image directement

--- src/createImageFromCheckbox/GridPaneWithCheckbox.java
+++ src/createImageFromCheckbox/GridPaneWithCheckbox.java
@@ -1,13 +1,17 @@
 package createImageFromCheckbox;
 
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class GridPaneWithCheckbox extends GridPane {
 	private int largeur; 
 	private int longueur; 
-	private CheckBox[][] cbMatrix; 
+	private Label[][] cbMatrix; 
 	private int[][] intMatrix; 
+	private int xCoord; 
+	private int yCoord;
+	private int[][] colorMatrix; 
 
 	public GridPaneWithCheckbox(int largeur, int longueur) {
 		super();
@@ -15,28 +19,60 @@ public class GridPaneWithCheckbox extends GridPane {
 		this.largeur = largeur; 
 		this.longueur= longueur; 
 		
-		this.cbMatrix =  new CheckBox[this.largeur+1][this.longueur+1];
+		
+		this.cbMatrix =  new Label[this.largeur+1][this.longueur+1];
+		this.colorMatrix =  new int[this.largeur][this.longueur];
 		
 		for(int y = 0; y<largeur; y++) {
 			for(int x = 0; x<longueur; x++) {
-				cbMatrix[y][x] = new CheckBox();
+				this.xCoord = x; 
+				this.yCoord = y;
+				
+				Label tmp = new Label(); 
+				int tmpInt = 0; 
+				tmp.setMinSize(10, 10);
+				tmp.setMinSize(20, 20);
+				tmp.setStyle("-fx-border-color: black;");
+				
+				tmp.setOnMouseEntered(e->{
+					tmp.setStyle("-fx-background-color: grey");
+				});
+				tmp.setOnMouseExited(e-> {
+					if(colorMatrix[GridPane.getRowIndex(tmp)][GridPane.getColumnIndex(tmp)] == 1) {
+						tmp.setStyle("-fx-background-color: black");
+					}else {
+					tmp.setStyle("-fx-background-color: white; -fx-border-color: black; ");
+					}
+				});
+				
+				tmp.setOnMouseClicked(e-> {
+					System.out.println(this.yCoord + ";" + this.xCoord);
+					if(colorMatrix[GridPane.getRowIndex(tmp)][GridPane.getColumnIndex(tmp)] == 1) {
+						tmp.setStyle("-fx-background-color: white");
+						colorMatrix[GridPane.getRowIndex(tmp)][GridPane.getColumnIndex(tmp)] = 0;
+					}else {
+						colorMatrix[GridPane.getRowIndex(tmp)][GridPane.getColumnIndex(tmp)] = 1;
+						tmp.setStyle("-fx-background-color: black");
+					}
+					
+				});
+				
+				
+				colorMatrix[y][x] = tmpInt;
+				cbMatrix[y][x] = tmp;
 				this.add(cbMatrix[y][x], x, y);
 			}
 		}
 			
 	}
-	public int[][] getMatrix() {
-		this.intMatrix = new int[this.largeur][this.longueur]; 
-		
-		for(int y = 0; y<largeur; y++) {
-			for(int x = 0; x<longueur; x++) {
-				if(cbMatrix[y][x].isSelected()) {
-					intMatrix[y][x] = 1; 
-				}	
+	public int[][] getMatrix() {	
+		for(int[] ligne : this.colorMatrix) {
+			for(int val : ligne) {
+				System.out.print(val);
 			}
+			System.out.println();
 		}
-		
-		return this.intMatrix;
+		return this.colorMatrix;
 	}
 
 }
